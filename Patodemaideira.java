0e1c9cc509de66a6ef79ef3cebd3384a8e9f8338
Yurek4 <147882154+Yurek4@users.noreply.github.com>
2024-03-25 19:01:57 -0300

    Create Patodemaideira.java

---
+++ Patodemaideira.java
@@ -0,0 +1,16 @@
+
+public class PatoMadeira extends Pato{
+	public PatoMadeira(String umNome, int umAno ){
+		super(umNome, umAno);
+	}
+	public void voa() {
+		System.out.println("O Pato não voa");
+	}
+	public void nada() {
+		System.out.println("O Pato não nada");
+	}
+	public void quack() {
+		System.out.println(".....");
+	}
+	
+}
